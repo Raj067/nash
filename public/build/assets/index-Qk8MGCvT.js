4b3b22253cebc98c5c8320f30225ba5051ae48d1
Rajabu Mrisho <75034394+Raj067@users.noreply.github.com>
2025-09-23 21:25:46 +0300

    ---

--- public/build/assets/index-Qk8MGCvT.js
+++
@@ -1,6 +0,0 @@
-import{c as h}from"./createLucideIcon-p61r0S7D.js";import{r as n}from"./app-CJ_dDD88.js";import{u}from"./index-Dy_jmIk6.js";/**
- * @license lucide-react v0.544.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const b=[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]],g=h("check",b);function p(r){const[d,e]=n.useState(void 0);return u(()=>{if(r){e({width:r.offsetWidth,height:r.offsetHeight});const c=new ResizeObserver(o=>{if(!Array.isArray(o)||!o.length)return;const f=o[0];let i,t;if("borderBoxSize"in f){const s=f.borderBoxSize,a=Array.isArray(s)?s[0]:s;i=a.inlineSize,t=a.blockSize}else i=r.offsetWidth,t=r.offsetHeight;e({width:i,height:t})});return c.observe(r,{box:"border-box"}),()=>c.unobserve(r)}else e(void 0)},[r]),d}export{g as C,p as u};
