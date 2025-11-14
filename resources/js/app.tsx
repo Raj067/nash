bafbc2df36c10a3458eacef5bbe6b325bd62e6c7
Rajabu Mrisho <75034394+Raj067@users.noreply.github.com>
2025-11-14 09:37:54 +0300

    ---

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,19 +1,21 @@
-import '../css/app.css';
-import './bootstrap';
-import './i18n';
+import "../css/app.css";
+import "./bootstrap";
+import "./i18n";
 
-import { createInertiaApp } from '@inertiajs/react';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { createRoot, hydrateRoot } from 'react-dom/client';
+import { createInertiaApp } from "@inertiajs/react";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { createRoot, hydrateRoot } from "react-dom/client";
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName =
+    import.meta.env.VITE_APP_NAME ||
+    "NATIONAL AIDS, STIs AND HEPATITIS CONTROL PROGRAMME";
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) =>
         resolvePageComponent(
             `./Pages/${name}.tsx`,
-            import.meta.glob('./Pages/**/*.tsx'),
+            import.meta.glob("./Pages/**/*.tsx")
         ),
     setup({ el, App, props }) {
         if (import.meta.env.SSR) {
@@ -24,6 +26,6 @@ createInertiaApp({
         createRoot(el).render(<App {...props} />);
     },
     progress: {
-        color: '#4B5563',
+        color: "#4B5563",
     },
 });
